69e23ed5d04a41e8208a0a1c46100796ca675754
Shmotten <sam@samotten.eu>
2018-03-27 22:07:28 +0200

    A prototype for the abar

---
+++ new_abar/abar.php
@@ -0,0 +1,14 @@
+<!-- Advertisment bar -->
+<script>
+    
+    // Mobile advertisment
+    if (screen.width < 480) {
+        
+    }
+    
+    // Desktop advertisment
+    else {
+        
+    }
+</script>
+<script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
